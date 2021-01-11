0b767c77843d286832b2a72702af5847c079c2bc
Braxton Shoop <braxton.shoop@gmail.com>
2021-01-11 14:47:44 -0600

    Gifter exercise 2 completed

---
+++ Gifter/Models/Comment.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Gifter.Models
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+
+        public string UserProfileId { get; set; }
+
+        public int PostId { get; set; }
+
+        public string Message { get; set; }
+    }
+}
---
+++ Gifter/Repositories/CommentRepository.cs
@@ -0,0 +1,57 @@
+﻿using Gifter.Data;
+using Gifter.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Gifter.Repositories
+{
+    public class CommentRepository : ICommentRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CommentRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public List<Comment> GetAll()
+        {
+            return _context.Comment
+                .ToList();
+        }
+
+        public Comment GetById(int id)
+        {
+            return _context.Comment
+                .FirstOrDefault(p => p.Id == id);
+        }
+
+        public Comment GetByPostId(int id)
+        {
+            return _context.Comment
+                .FirstOrDefault(p => p.Id == id);
+        }
+
+        public void Add(Comment comment)
+        {
+            _context.Add(comment);
+            _context.SaveChanges();
+        }
+
+        public void Update(Comment comment)
+        {
+            _context.Entry(comment).State = EntityState.Modified;
+            _context.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            var comment = GetById(id);
+            _context.Comment.Remove(comment);
+            _context.SaveChanges();
+        }
+    }
+}

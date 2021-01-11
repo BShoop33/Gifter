b6f2d9f8ea23f8232da127d6bcf3c5e61852759c
Braxton Shoop <braxton.shoop@gmail.com>
2021-01-11 11:49:42 -0600

    beginning Gifter exercise 1

--- Gifter/Data/ApplicationDbContext.cs
+++ Gifter/Data/ApplicationDbContext.cs
@@ -1,11 +1,21 @@
-﻿using System;
+﻿using Gifter.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Gifter.Data
 {
-    public class ApplicationDbContext
+    //This is the C# representation of our whole database
+    //The DBContext class that this class inherits from is a built-in entity framework class
+    public class ApplicationDbContext : DbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        //The DbSet object type represents the entire database to which it is set
+        public DbSet<UserProfile> UserProfile { get; set; }
+
+        public DbSet<Post> Post { get; set; }
     }
 }
--- Gifter/Repositories/IPostRepository.cs
+++ Gifter/Repositories/IPostRepository.cs
@@ -5,7 +5,10 @@ namespace Gifter.Repositories
 {
     public interface IPostRepository
     {
+        void Add(Post post);
         List<Post> GetAll();
         Post GetById(int id);
+        void Update(Post post);
+        void Delete(int id);
     }
 }
